de4b903e3acdd017f15c4f1cdbf962ab26651ae0
liam-kilsdonk <liamkilsdonk@gmail.com>
2023-11-05 22:26:25 +0100

    voor laptop

---
+++ src/app/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./page.module.css";
+
+function Footer() {
+  return (
+    <footer>
+      <div className={styles.footer}>
+        <p>This is the footer.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
