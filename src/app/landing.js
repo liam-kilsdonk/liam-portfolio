dfdd13174e4dcb4f403c16022d8bd21c782fb69d
Liam Kilsdonk <liamkilsdonk@gmail.com>
2024-02-04 14:20:27 +0100

    ja man

--- src/app/landing.js
+++ src/app/landing.js
@@ -1,6 +1,7 @@
 'use client'
 import React from "react";
 import styles from "./page.module.css";
+import CircularProgress from '@mui/material/CircularProgress';
 
 function Landing() {
 
@@ -18,6 +19,7 @@ function Landing() {
                     type="button"
                     onClick={() => setColor(color + 1)}
                 >Blue</button>
+                <CircularProgress />
             </div>
             <div className={styles.landing_right}>
                 <img
--- src/app/slider.js
+++ src/app/slider.js
@@ -1,10 +1,40 @@
-import React from 'react';
+'use client'
+//import React from 'react';
 import styles from './page.module.css';
+import React, { useState, useEffect } from 'react';
 
 function Slider(){
     return(
         <section>
             <div className={styles.slider_wrap}>
+               <div className={styles.slider_top}>
+                <span className={styles.slider_right}>Innovation</span>
+                <span className={styles.slider_right}>Creativity</span>
+                <span className={styles.slider_right}>Efficiency</span>
+                <span className={styles.slider_right}>Problem-solving</span>
+                <span className={styles.slider_right}>Collaboration</span>
+                <span className={styles.slider_right}>Quality</span>
+                <span className={styles.slider_right}>Precision</span>
+                <span className={styles.slider_right}>Agility</span>
+                <span className={styles.slider_right}>Excellence</span>
+                <span className={styles.slider_right}>Reliability</span>
+                <span className={styles.slider_right}>Adaptability</span>
+                
+               </div>
+               <div className={styles.slider_top}>
+                <span className={styles.slider_left}>Design</span>
+                <span className={styles.slider_left}>lorem</span>
+                <span className={styles.slider_left}>hello</span>
+                <span className={styles.slider_left}>hello</span>
+                <span className={styles.slider_left}>Design</span>
+                <span className={styles.slider_left}>lorem</span>
+                <span className={styles.slider_left}>hello</span>
+                <span className={styles.slider_left}>hello</span>
+                <span className={styles.slider_left}>Design</span>
+                <span className={styles.slider_left}>lorem</span>
+                <span className={styles.slider_left}>hello</span>
+               </div>
+
                <div className={styles.slider_top}>
                 <span className={styles.slider_right}>Design</span>
                 <span className={styles.slider_right}>lorem</span>
@@ -19,7 +49,8 @@ function Slider(){
                 <span className={styles.slider_right}>hello</span>
                 
                </div>
-               <div className={styles.slider_middle}>
+               
+               {/* <div className={styles.slider_middle}>
                  <span className={styles.slider_left}>hello</span>
                  <span className={styles.slider_left}>hello</span>
                  <span className={styles.slider_left}>hello</span>
@@ -34,7 +65,7 @@ function Slider(){
                <div id={styles.sliderContainer}>
                     <div class={styles.sliderText} id={styles.slider_test_1}>Your Text Here</div>
                     <div class={styles.sliderText} id={styles.slider_test_2}>Your Text Here</div>
-                </div>
+                </div> */}
             </div>
         </section>
     );
